e6cd05fbacf597d7cea4f46b24f02f383133e4ae
Alexander Medved <medved1991alexander@gmail.com>
2024-07-25 18:02:15 +0300

    add smooth page navigation

--- src/script.js
+++ src/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+//*** MODAL WINDOW ***
 const modalWindow = document.querySelector('.modal-window');
 const overlay = document.querySelector('.overlay');
 const btnCloseModalWindow = document.querySelector('.btn--close-modal-window');
@@ -33,10 +31,11 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+
+//*** SCROLL PAGE ***
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
-//*** SCROLL PAGE ***
 btnScrollTo.addEventListener('click', function(e){
   const section1Position = section1.getBoundingClientRect();
 
@@ -51,3 +50,58 @@ btnScrollTo.addEventListener('click', function(e){
   section1.scrollIntoView({ behavior: 'smooth'});
 
 });
+
+
+//*** SMOOTH PAGE NAVIGATION ***
+//bad approach
+/*
+document.querySelectorAll('.nav__link')
+  .forEach(function(htmlElement){
+    htmlElement.addEventListener('click', function(e){
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({behavior: 'smooth'});    
+    });
+  });*/
+
+//good approach "delegate event"
+//1. Add event listener for common parent.
+document.querySelector('.nav__links').addEventListener('click', function(e){
+  e.preventDefault();
+  //2. Define target element.
+  if (e.target.classList.contains('nav__link')){
+    document.querySelector(e.target.getAttribute('href'))
+    .scrollIntoView({behavior: 'smooth'});
+  }
+});
+
+
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+/*
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const getRandomColor = () => 
+  `rgb(${getRandomIntInclusive(0, 255)},
+  ${getRandomIntInclusive(0, 255)},
+  ${getRandomIntInclusive(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function(e) {
+  e.preventDefault();
+  this.style.backgroundColor = getRandomColor();
+  e.stopPropagation();
+
+});
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  this.style.backgroundColor = getRandomColor();
+});
+
+document.querySelector('.nav').addEventListener('click', function(e) {
+  this.style.backgroundColor = getRandomColor();
+},true);*/
